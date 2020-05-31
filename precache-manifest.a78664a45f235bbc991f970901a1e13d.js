2318c9929936376ef1a92fe22a6f0e807d170b74
DenMinzalirov <den.minzakirov@gmail.com>
2020-05-31 20:28:28 +0300

    v0.1

---
+++ precache-manifest.a78664a45f235bbc991f970901a1e13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01055050d6db8b20ae83dff66016149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c405922e009f30a3a8b",
+    "url": "/static/css/main.52c9507a.chunk.css"
+  },
+  {
+    "revision": "f900201e38b95b1a59c8",
+    "url": "/static/js/2.62ded9ea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.62ded9ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c405922e009f30a3a8b",
+    "url": "/static/js/main.ae98baeb.chunk.js"
+  },
+  {
+    "revision": "3b92c4f641b2dcd8a32b",
+    "url": "/static/js/runtime-main.c87c31af.js"
+  }
+]);
